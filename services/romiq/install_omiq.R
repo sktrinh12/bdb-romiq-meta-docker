01620747c4b3da7f8df1844283b51d1283df75c9
sktrinh12 <trinh.kcs85@gmail.com>
2020-12-31 01:19:05 -0800

    removed install_omiq.R since cloning it from github

--- services/romiq/install_omiq.R
+++
@@ -1,7 +0,0 @@
-dirpath <- paste0("/home/", Sys.getenv("UNAME"))
-version <- names(read.csv(paste0(dirpath,"/env.version.tmp")))
-length_str <- nchar(version)
-version <- substr(version, 2,length_str)
-filepath <- paste0(dirpath, "/R/omiq_v", version, "/OmiqPipeline")
-install.packages(filepath, repos=NULL, type="source")
-
