ffacb6d3be45658fb29cabb4c2431be8db6b2598
sktrinh12 <trinh.kcs85@gmail.com>
2021-01-03 16:53:51 -0800

    adde debug env var

--- run_svr.sh
+++ run_svr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export FLASK_APP=$PWD/services/project/__init__.py
+export FLASK_APP=$PWD/services/pyapi/project/__init__.py
+export FLASK_DEBUG=1
 source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate py38
 python $PWD/services/pyapi/manage.py run
